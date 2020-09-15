3d364a7e18897fea20617ba727c82b2ef947ca04
Grant Rudow <webdev.grant@gmail.com>
2020-09-14 21:04:17 -0500

    initial commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'a30cd77165f8a026e7ed467554c9c46c'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '64a475be-0112-4d04-9005-f06189ae65fa'
+};
